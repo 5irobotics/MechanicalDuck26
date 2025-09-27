28d0ca40b750eb0861bcccd3253dc6b53f0e0ad9
D035iRobotics <D03@5irobotics.org>
2025-09-27 17:00:13 -0500

    We added limelight functionality to test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeasonCode/TeleOp/subsystems/LimeLight1.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.SeasonCode.TeleOp.subsystems;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+@Autonomous
+public class LimeLight1 extends OpMode {
+
+    private Limelight3A limelight3A;
+
+    @Override
+    public void init(){
+        limelight3A = hardwareMap.get(Limelight3A.class,"limelight");
+
+        double x = 0;
+
+
+        limelight3A.pipelineSwitch(4 ); //3 is April tag 20, 4 is 21, 5 is 22, 6 is 23, 7 is 24
+    }
+
+    @Override
+    public void start(){
+        limelight3A.start();
+    }
+
+    @Override
+    public void loop() {
+        LLResult llResult = limelight3A.getLatestResult();
+        if(llResult != null && llResult.isValid()){
+            telemetry.addData("Target X offset", llResult.getTx());
+            telemetry.addData("Target Y offset", llResult.getTy());
+            telemetry.addData("Target Area offset", llResult.getTa());
+        }
+    }
+
+}
+
+
+
